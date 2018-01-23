bec66ccdd66df0340f07949e72f648f94e62de2e
zenia <egen1um@yahoo.com>
2018-01-23 11:40:58 +0200

    no message

---
+++ SetStuffActive.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStuffActive : MonoBehaviour
+{
+    public List<GameObject> stuffToActivate;
+    public List<GameObject> stuffToDeactivate;
+
+    public void Execute()
+    {
+        stuffToActivate.ForEach(go => go.SetActive(true));
+        stuffToDeactivate.ForEach(go => go.SetActive(false));
+    }
+}
