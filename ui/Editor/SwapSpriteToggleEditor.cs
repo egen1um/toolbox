f68b88c3dd66023b02080fd3778855604220f2df
zenia <egen1um@yahoo.com>
2018-02-07 12:00:35 +0200

    no message

---
+++ ui/Editor/SwapSpriteToggleEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(SwapSpriteToggle))]
+public class SwapSpriteToggleEditor : ToggleEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_isOnSprite"));
+        serializedObject.ApplyModifiedProperties();
+    }
+}
